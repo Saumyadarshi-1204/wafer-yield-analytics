4e3cd60777061bda525568b1134a859688c04e7b
Saumyadarshi-1204 <saumyadarshi0@gmail.com>
2026-01-13 09:32:56 +0530

    Add image to check working

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <img src="/images/React_logo.webp" alt="React logo" />
       <h1>Wafer Yield Analytics System</h1>
       <br />
       <p>React is working correctly.</p>
